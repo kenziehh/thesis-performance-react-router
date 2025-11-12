a01607d913196984a2b33b76dae69333d8d131eb
kenziehh <ghaisani.nurani@gmail.com>
2025-11-12 13:34:12 +0700

    feat: add format date utils

--- app/shared/lib/format.ts
+++ app/shared/lib/format.ts
@@ -5,3 +5,12 @@ export function formatIDR(n: number) {
     maximumFractionDigits: 0,
   }).format(n)
 }
+
+export function formatDate (date: string) {
+  const d = new Date(date)
+  return d.toLocaleDateString("id-ID", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  })
+}
