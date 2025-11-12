429f161c3a1866bcc73f012015a034afaedfe470
kenziehh <ghaisani.nurani@gmail.com>
2025-11-12 13:35:04 +0700

    chore: remove comments and remove unused dockerfile dockerignore

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
-  async prerender(args) {
-    return ["/", "/ecommerce", "/news"];
-  },
+ssr: true,
+async prerender(args) {
+  return ["/", "/ecommerce", "/news"];
+},
 } satisfies Config;
